e5a6811031c22c0fc5a48b108c5a37221730c585
ranghel <anghelramona30@gmail.com>
2017-05-22 10:50:47 -0400

    trying to fix heroku issue

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static("public"));
 
 // Database configuration with mongoose
 var databaseUri = "mongodb://localhost/mongoosearticles";
-var MONGO_URI = 'mongodb://heroku_hq7f8q8k:vc3n061u3eb94skprj66n4u2a5@ds149201.mlab.com:49201/heroku_hq7f8q8k"
+var MONGO_URI = "mongodb://heroku_hq7f8q8k:vc3n061u3eb94skprj66n4u2a5@ds149201.mlab.com:49201/heroku_hq7f8q8k";
 
 if(process.env.MONGODB_URI)
 {
